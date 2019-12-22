e33089f17642c3af1b940cf0bc416d9373f23582
Gabriele2396 <56448402+Gabriele2396@users.noreply.github.com>
2019-12-22 20:16:16 +0100

    main.py file

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+
+if __name__ == '__main__':
+
+    function = int(input("Please insert a number from 1 to 4 to choose your function: ")) # user can choose the function to run.
+
+    if func == 1:
+
+        # here we insert our function
+
+    elif func == 2:
+
+        # here we insert our function
+    
+    elif func == 3:
+
+        # here we insert our function
+    
+    elif func == 4:
+
+        # here we insert our function
